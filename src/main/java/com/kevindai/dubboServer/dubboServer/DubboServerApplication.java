ea24573d2a95ee7d9a2b5902660b911dc5016cd6
kevindai007 <741007187@qq.com>
2017-06-06 07:55:50 +0800

    duboo server完成

--- src/main/java/com/kevindai/dubboServer/dubboServer/DubboServerApplication.java
+++ src/main/java/com/kevindai/dubboServer/dubboServer/DubboServerApplication.java
@@ -3,9 +3,11 @@ package com.kevindai.dubboServer.dubboServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
 @ComponentScan("com.kevindai")
+@ImportResource("classpath:dubbo-server.xml")
 public class DubboServerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/kevindai/service/impl/MsgServiceImpl.java
+++ src/main/java/com/kevindai/service/impl/MsgServiceImpl.java
@@ -1,14 +1,14 @@
 package com.kevindai.service.impl;
 
-import com.alibaba.dubbo.config.annotation.Service;
 import com.kevindai.service.MsgService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by Administrator on 2017/6/5 0005.
  */
-@Service(version = "1.0.0")
+@Service("msgService")
 public class MsgServiceImpl implements MsgService {
     private static final Logger LOGGER = LoggerFactory.getLogger(MsgServiceImpl.class);
     @Override
